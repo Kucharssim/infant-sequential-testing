d311fb97f3591af73ddc8a941b88de2b52d9890f
Kucharssim <kucharssim@gmail.com>
2022-06-15 17:23:41 +0200

    rerun default prior

--- scripts/single-main-effect.R
+++ scripts/single-main-effect.R
@@ -1,4 +1,5 @@
 library(BFDA)
+sink(file = "default-prior.Rout")
 
 # Global variables ----
 # The following example assumes an uninformed "default" prior, but one-sided
@@ -7,22 +8,24 @@ prior_location <- 0
 prior_scale    <- sqrt(2)/2
 prior_alternative <- list("Cauchy", list(prior.location = prior_location, prior.scale = prior_scale))
 
-d_of_interest  <- 0.5
+d_of_interest  <- 0.45
 
 min_n <- 5
 max_n <- 50
 step_size <- 5
 
 evidence_boundary <- 10
+verbose <- FALSE
 # Using BFDA package ----
 ## fixed N ----
+cat("Fixed N design\n\n")
 bfda_fixed_h0 <- BFDA.sim(expected.ES = 0,
                           type        = "t.paired",
                           prior       = prior_alternative,
                           alternative = "greater",
                           design      = "fixed.n",
                           n.max       = max_n,
-                          verbose     = TRUE)
+                          verbose     = verbose)
 
 bfda_fixed_h1 <- BFDA.sim(expected.ES = d_of_interest,
                           type        = "t.paired",
@@ -30,12 +33,15 @@ bfda_fixed_h1 <- BFDA.sim(expected.ES = d_of_interest,
                           alternative = "greater",
                           design      = "fixed.n",
                           n.max       = max_n,
-                          verbose     = TRUE)
+                          verbose     = verbose)
+cat("Fixed N design under null \n")
 BFDA.analyze(bfda_fixed_h0, design = "fixed", n = max_n, boundary = evidence_boundary)
+cat("Fixed N design under alternative \n")
 BFDA.analyze(bfda_fixed_h1, design = "fixed", n = max_n, boundary = evidence_boundary)
 BFDA::evDens(BFDA.H1 = bfda_fixed_h1, BFDA.H0 = bfda_fixed_h0, n = max_n, boundary = evidence_boundary)
 
 ## fixed N calculate desired sample size ----
+cat("Fixed N design, calculate desired sample size \n\n")
 desired_percentage <- 0.8
 current_percentage <- 0
 current_n <- max_n # we already know that for max_n we have less "power", so we start there
@@ -48,7 +54,7 @@ while(current_percentage < desired_percentage) {
                    alternative = "greater",
                    design      = "fixed.n",
                    n.max       = current_n,
-                   verbose     = FALSE,
+                   verbose     = verbose,
                    cores       = 4)
 
   current_percentage <- mean(exp(bfda$sim$logBF) > evidence_boundary)
@@ -66,10 +72,12 @@ bfda_0 <- BFDA.sim(expected.ES = 0,
                    prior       = prior_alternative,
                    alternative = "greater",
                    design      = "fixed.n",
-                   n.max       = current_n)
+                   n.max       = current_n,
+                   verbose     = verbose)
 BFDA.analyze(bfda_0, design = "fixed", n = current_n, boundary = evidence_boundary)
 
 ## sequential ----
+cat("Sequential design \n\n")
 bfda_seq_h0 <- BFDA.sim(expected.ES = 0,
                         type        = "t.paired",
                         prior       = prior_alternative,
@@ -79,7 +87,8 @@ bfda_seq_h0 <- BFDA.sim(expected.ES = 0,
                         n.min       = min_n,
                         n.max       = max_n,
                         stepsize    = 1,
-                        cores       = 4)
+                        cores       = 4,
+                        verbose     = verbose)
 
 bfda_seq_h1 <- BFDA.sim(expected.ES = d_of_interest,
                         type        = "t.paired",
@@ -90,8 +99,11 @@ bfda_seq_h1 <- BFDA.sim(expected.ES = d_of_interest,
                         n.min       = min_n,
                         n.max       = max_n,
                         stepsize    = 1,
-                        cores       = 4)
+                        cores       = 4,
+                        verbose     = verbose)
+cat("Sequential design under null \n")
 BFDA.analyze(bfda_seq_h0, design = "sequential", n.min = min_n, n.max = max_n, boundary = evidence_boundary)
+cat("Sequential design under alternative \n")
 BFDA.analyze(bfda_seq_h1, design = "sequential", n.min = min_n, n.max = max_n, boundary = evidence_boundary)
 
 
@@ -101,6 +113,7 @@ plotBFDA(bfda_seq_h1, boundary = evidence_boundary, n.min = min_n, n.max = max_n
 mtext(expression("Under"~H[1]))
 
 ## sequential with stepsize ----
+cat("Sequential design with steps \n\n")
 bfda_seqstep_h0 <- BFDA.sim(expected.ES = 0,
                             type        = "t.paired",
                             prior       = prior_alternative,
@@ -110,7 +123,8 @@ bfda_seqstep_h0 <- BFDA.sim(expected.ES = 0,
                             n.min       = min_n,
                             n.max       = max_n,
                             stepsize    = step_size,
-                            cores       = 4)
+                            cores       = 4,
+                            verbose     = verbose)
 
 bfda_seqstep_h1 <- BFDA.sim(expected.ES = d_of_interest,
                             type        = "t.paired",
@@ -121,11 +135,15 @@ bfda_seqstep_h1 <- BFDA.sim(expected.ES = d_of_interest,
                             n.min       = min_n,
                             n.max       = max_n,
                             stepsize    = step_size,
-                            cores       = 4)
+                            cores       = 4,
+                            verbose     = verbose)
+cat("Sequential design with steps under null \n")
 BFDA.analyze(bfda_seqstep_h0, design = "sequential", n.min = min_n, n.max = max_n, boundary = evidence_boundary)
+cat("Sequential design with steps under alternative \n")
 BFDA.analyze(bfda_seqstep_h1, design = "sequential", n.min = min_n, n.max = max_n, boundary = evidence_boundary)
 plotBFDA(bfda_seqstep_h1, boundary = evidence_boundary, n.min = min_n, n.max = max_n)
 
+sink()
 
 # Add nice plots ----
 library(ggplot2)
